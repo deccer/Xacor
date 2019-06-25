503b9e70249ddd1effed2051d69edebbb554945c
deccer <decccer@outlook.com>
2019-06-25 02:36:04 +0200

    Add MathHelper

---
+++ Xacor/MathHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Xacor
+{
+    public static class MathHelper
+    {
+        [Pure]
+        public static int Clamp(int n, int min, int max)
+        {
+            return Math.Max(Math.Min(n, max), min);
+        }
+
+        [Pure]
+        public static float Clamp(float n, float min, float max)
+        {
+            return Math.Max(Math.Min(n, max), min);
+        }
+
+        [Pure]
+        public static float DegreesToRadians(float degrees)
+        {
+            const float degToRad = (float)Math.PI / 180.0f;
+            return degrees * degToRad;
+        }
+
+        [Pure]
+        public static float RadiansToDegrees(float radians)
+        {
+            const float radToDeg = 180.0f / (float)Math.PI;
+            return radians * radToDeg;
+        }
+    }
+}
