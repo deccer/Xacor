4bdb382b73b0fa2211974e919869f3c087e3f187
deccer <decccer@outlook.com>
2019-08-25 11:48:00 +0200

    Implement DX12GraphicsDevice

--- Xacor.Graphics.Api.DX12/DX12GraphicsDevice.cs
+++ Xacor.Graphics.Api.DX12/DX12GraphicsDevice.cs
@@ -1,10 +1,16 @@
-﻿using SharpDX.Direct3D12;
+﻿using SharpDX;
+using SharpDX.Direct3D12;
 
 namespace Xacor.Graphics.Api.DX12
 {
     internal class DX12GraphicsDevice : IGraphicsDevice
     {
         private readonly Device _nativeDevice;
+
+        public static implicit operator ComObject(DX12GraphicsDevice graphicsDevice)
+        {
+            return graphicsDevice._nativeDevice;
+        }
 
         public static implicit operator Device(DX12GraphicsDevice graphicsDevice)
         {
@@ -18,7 +24,15 @@ namespace Xacor.Graphics.Api.DX12
 
         public DX12GraphicsDevice()
         {
+#if DEBUG
+            DebugInterface.Get().EnableDebugLayer();
+#endif
             _nativeDevice = new Device();
+        }
+
+        public DescriptorHeap CreateDescriptorHeap(DescriptorHeapDescription descriptorHeapDescription)
+        {
+            return _nativeDevice.CreateDescriptorHeap(descriptorHeapDescription);
         }
     }
 }
