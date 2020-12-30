e16f84cdf48a93a9329c9c65cf3dbd59c9ac2b16
deccer <decccer@outlook.com>
2020-12-30 16:19:51 +0400

    Start integrating ImGui

---
+++ Demos/Xacor.Demo/ImGuiRenderer.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using ImGuiNET;
+using SharpDX.Direct2D1;
+using SharpDX.Direct3D11;
+using Xacor.Game;
+using Xacor.Graphics.Api;
+using Buffer = System.Buffer;
+using CullMode = Xacor.Graphics.Api.CullMode;
+using FillMode = Xacor.Graphics.Api.FillMode;
+using Num = System.Numerics;
+
+namespace Xacor.Demo
+{
+    /*
+    public sealed class ImGuiRenderer : IDisposable
+    {
+        private readonly IGraphicsFactory _graphicsFactory;
+        private readonly ITextureFactory _textureFactory;
+        private readonly IGraphicsDevice _graphicsDevice;
+
+        private IShader _effect;
+        private readonly IRasterizerState _rasterizerState;
+
+        private byte[] _vertexData;
+        private IVertexBuffer _vertexBuffer;
+        private int _vertexBufferSize;
+
+        private byte[] _indexData;
+        private IIndexBuffer _indexBuffer;
+        private int _indexBufferSize;
+
+        private readonly Dictionary<IntPtr, ITexture> _loadedTextures;
+
+        private ITexture _fontAtlasTexture;
+        private int _textureId;
+        private IntPtr? _fontAtlasTextureId;
+        private int _scrollWheelValue;
+
+        private readonly List<int> _keys = new List<int>();
+
+        public ImGuiRenderer(GameBase game, IGraphicsFactory graphicsFactory, ITextureFactory textureFactory)
+        {
+            _graphicsFactory = graphicsFactory;
+            _textureFactory = textureFactory;
+            var context = ImGui.CreateContext();
+            ImGui.SetCurrentContext(context);
+
+            _graphicsDevice = game.GraphicsDevice;
+
+            _loadedTextures = new Dictionary<IntPtr, ITexture>();
+
+            _rasterizerState = _graphicsFactory.CreateRasterizerState(
+                CullMode.None,
+                FillMode.Solid,
+                false,
+                true,
+                false,
+                false);
+
+            var io = ImGui.GetIO();
+            io.ConfigFlags = ImGuiConfigFlags.DockingEnable;
+            SetupInput(io);
+            //SetRedStyle(ImGui.GetStyle());
+        }
+
+        public unsafe void RebuildFontAtlas()
+        {
+            var io = ImGui.GetIO();
+            io.Fonts.GetTexDataAsRGBA32(out byte* pixelData, out var width, out var height, out var bytesPerPixel);
+
+            var pixels = new byte[width * height * bytesPerPixel];
+            Marshal.Copy(new IntPtr(pixelData), pixels, 0, pixels.Length);
+
+            _fontAtlasTexture?.Dispose();
+            _fontAtlasTexture = _textureFactory.CreateTexture(width, height, Format.R8G8B8UNorm, false);
+            _fontAtlasTexture.SetData(pixels);
+
+            if (_fontAtlasTextureId.HasValue)
+            {
+                UnbindTexture(_fontAtlasTextureId.Value);
+            }
+
+            _fontAtlasTextureId = BindTexture(_fontAtlasTexture);
+            io.Fonts.SetTexID(_fontAtlasTextureId.Value);
+            io.Fonts.ClearTexData();
+        }
+
+        public IntPtr BindTexture(ITexture texture)
+        {
+            var id = new IntPtr(_textureId++);
+
+            _loadedTextures.Add(id, texture);
+
+            return id;
+        }
+
+        public void Dispose()
+        {
+            _fontAtlasTexture?.Dispose();
+        }
+
+        public void UnbindTexture(IntPtr textureId)
+        {
+            _loadedTextures.Remove(textureId);
+        }
+
+        public void BeginLayout(float gameTime)
+        {
+            ImGui.GetIO().DeltaTime = gameTime;
+
+            UpdateInput();
+
+            ImGui.NewFrame();
+        }
+
+        public void EndLayout()
+        {
+            ImGui.Render();
+
+            RenderDrawData(ImGui.GetDrawData());
+        }
+
+        private void SetupInput(ImGuiIOPtr io)
+        {
+            _keys.Add(io.KeyMap[(int)ImGuiKey.Tab] = (int)Keys.Tab);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.LeftArrow] = (int)Keys.Left);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.RightArrow] = (int)Keys.Right);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.UpArrow] = (int)Keys.Up);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.DownArrow] = (int)Keys.Down);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.PageUp] = (int)Keys.PageUp);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.PageDown] = (int)Keys.PageDown);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.Home] = (int)Keys.Home);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.End] = (int)Keys.End);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.Delete] = (int)Keys.Delete);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.Backspace] = (int)Keys.Back);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.Enter] = (int)Keys.Enter);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.Escape] = (int)Keys.Escape);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.A] = (int)Keys.A);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.C] = (int)Keys.C);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.V] = (int)Keys.V);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.X] = (int)Keys.X);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.Y] = (int)Keys.Y);
+            _keys.Add(io.KeyMap[(int)ImGuiKey.Z] = (int)Keys.Z);
+
+
+            TextInputEXT.TextInput += c =>
+            {
+                if (c == '\t')
+                {
+                    return;
+                }
+
+                ImGui.GetIO().AddInputCharacter(c);
+            };
+
+
+            ImGui.GetIO().Fonts.AddFontDefault();
+        }
+
+        protected Effect UpdateEffect(Texture2D texture)
+        {
+            _effect ??= new BasicEffect(_graphicsDevice);
+
+            var io = ImGui.GetIO();
+            var offset = 0f;
+
+            _effect.World = Matrix.Identity;
+            _effect.View = Matrix.Identity;
+            _effect.Projection = Matrix.CreateOrthographicOffCenter(
+                offset,
+                io.DisplaySize.X + offset,
+                io.DisplaySize.Y + offset,
+                offset,
+                -1f,
+                1f
+            );
+            _effect.TextureEnabled = true;
+            _effect.Texture = texture;
+            _effect.VertexColorEnabled = true;
+
+            return _effect;
+        }
+
+        public void UpdateInput()
+        {
+            var io = ImGui.GetIO();
+
+            var mouse = Mouse.GetState();
+            var keyboard = Keyboard.GetState();
+
+            for (var i = 0; i < _keys.Count; i++)
+            {
+                io.KeysDown[_keys[i]] = keyboard.IsKeyDown((Keys)_keys[i]);
+            }
+
+            io.KeyShift = keyboard.IsKeyDown(Keys.LeftShift) || keyboard.IsKeyDown(Keys.RightShift);
+            io.KeyCtrl = keyboard.IsKeyDown(Keys.LeftControl) || keyboard.IsKeyDown(Keys.RightControl);
+            io.KeyAlt = keyboard.IsKeyDown(Keys.LeftAlt) || keyboard.IsKeyDown(Keys.RightAlt);
+            io.KeySuper = keyboard.IsKeyDown(Keys.LeftWindows) || keyboard.IsKeyDown(Keys.RightWindows);
+
+            io.DisplaySize = new Num.Vector2(
+                _graphicsDevice.PresentationParameters.BackBufferWidth,
+                _graphicsDevice.PresentationParameters.BackBufferHeight
+            );
+            io.DisplayFramebufferScale = new Num.Vector2(1f, 1f);
+
+            io.MousePos = new Num.Vector2(mouse.X, mouse.Y);
+
+            io.MouseDown[0] = mouse.LeftButton == ButtonState.Pressed;
+            io.MouseDown[1] = mouse.RightButton == ButtonState.Pressed;
+            io.MouseDown[2] = mouse.MiddleButton == ButtonState.Pressed;
+
+            var scrollDelta = mouse.ScrollWheelValue - _scrollWheelValue;
+            io.MouseWheel = scrollDelta > 0
+                ? 1
+                : scrollDelta < 0
+                    ? -1
+                    : 0;
+            _scrollWheelValue = mouse.ScrollWheelValue;
+        }
+
+        private void RenderDrawData(ImDrawDataPtr drawData)
+        {
+            // Setup render state: alpha-blending enabled, no face culling, no depth testing, scissor enabled, vertex/texcoord/color pointers
+            var lastViewport = _graphicsDevice.Viewport;
+            var lastScissorBox = _graphicsDevice.ScissorRectangle;
+
+            _graphicsDevice.BlendFactor = Color.White;
+            _graphicsDevice.BlendState = BlendState.NonPremultiplied;
+            _graphicsDevice.RasterizerState = _rasterizerState;
+            _graphicsDevice.DepthStencilState = DepthStencilState.DepthRead;
+
+            // Handle cases of screen coordinates != from framebuffer coordinates (e.g. retina displays)
+            drawData.ScaleClipRects(ImGui.GetIO().DisplayFramebufferScale);
+
+            // Setup projection
+            _graphicsDevice.Viewport = new Viewport(
+                0,
+                0,
+                _graphicsDevice.PresentationParameters.BackBufferWidth,
+                _graphicsDevice.PresentationParameters.BackBufferHeight
+            );
+
+            UpdateBuffers(drawData);
+
+            RenderCommandLists(drawData);
+
+            // Restore modified state
+            _graphicsDevice.Viewport = lastViewport;
+            _graphicsDevice.ScissorRectangle = lastScissorBox;
+        }
+
+        private unsafe void UpdateBuffers(ImDrawDataPtr drawData)
+        {
+            if (drawData.TotalVtxCount == 0)
+            {
+                return;
+            }
+
+            if (drawData.TotalVtxCount > _vertexBufferSize)
+            {
+                _vertexBuffer?.Dispose();
+
+                _vertexBufferSize = (int)(drawData.TotalVtxCount * 1.5f);
+                _vertexBuffer = new VertexBuffer(
+                    _graphicsDevice,
+                    ImGuiDrawVertexDeclaration.Declaration,
+                    _vertexBufferSize,
+                    BufferUsage.None
+                );
+                _vertexData = new byte[_vertexBufferSize * ImGuiDrawVertexDeclaration.Size];
+            }
+
+            if (drawData.TotalIdxCount > _indexBufferSize)
+            {
+                _indexBuffer?.Dispose();
+
+                _indexBufferSize = (int)(drawData.TotalIdxCount * 1.5f);
+                _indexBuffer = new IndexBuffer(_graphicsDevice, IndexElementSize.SixteenBits, _indexBufferSize, BufferUsage.None);
+                _indexData = new byte[_indexBufferSize * sizeof(ushort)];
+            }
+
+            // Copy ImGui's vertices and indices to a set of managed byte arrays
+            var vtxOffset = 0;
+            var idxOffset = 0;
+
+            for (var n = 0; n < drawData.CmdListsCount; n++)
+            {
+                var cmdList = drawData.CmdListsRange[n];
+
+                fixed (void* vtxDstPtr = &_vertexData[vtxOffset * ImGuiDrawVertexDeclaration.Size])
+                {
+                    fixed (void* idxDstPtr = &_indexData[idxOffset * sizeof(ushort)])
+                    {
+                        Buffer.MemoryCopy(
+                            (void*)cmdList.VtxBuffer.Data,
+                            vtxDstPtr,
+                            _vertexData.Length,
+                            cmdList.VtxBuffer.Size * ImGuiDrawVertexDeclaration.Size
+                        );
+                        Buffer.MemoryCopy(
+                            (void*)cmdList.IdxBuffer.Data,
+                            idxDstPtr,
+                            _indexData.Length,
+                            cmdList.IdxBuffer.Size * sizeof(ushort)
+                        );
+                    }
+                }
+
+                vtxOffset += cmdList.VtxBuffer.Size;
+                idxOffset += cmdList.IdxBuffer.Size;
+            }
+
+            // Copy the managed byte arrays to the gpu vertex- and index buffers
+            _vertexBuffer.SetData(_vertexData, 0, drawData.TotalVtxCount * ImGuiDrawVertexDeclaration.Size);
+            _indexBuffer.SetData(_indexData, 0, drawData.TotalIdxCount * sizeof(ushort));
+        }
+
+        private unsafe void RenderCommandLists(ImDrawDataPtr drawData)
+        {
+            _graphicsDevice.SetVertexBuffer(_vertexBuffer);
+            _graphicsDevice.Indices = _indexBuffer;
+
+            var vtxOffset = 0;
+            var idxOffset = 0;
+
+            for (var n = 0; n < drawData.CmdListsCount; n++)
+            {
+                var cmdList = drawData.CmdListsRange[n];
+
+                for (var cmdi = 0; cmdi < cmdList.CmdBuffer.Size; cmdi++)
+                {
+                    var drawCmd = cmdList.CmdBuffer[cmdi];
+
+                    if (!_loadedTextures.ContainsKey(drawCmd.TextureId))
+                    {
+                        throw new InvalidOperationException(
+                            $"Could not find a texture with id '{drawCmd.TextureId}', please check your bindings"
+                        );
+                    }
+
+                    _graphicsDevice.ScissorRectangle = new Rectangle(
+                        (int)drawCmd.ClipRect.X,
+                        (int)drawCmd.ClipRect.Y,
+                        (int)(drawCmd.ClipRect.Z - drawCmd.ClipRect.X),
+                        (int)(drawCmd.ClipRect.W - drawCmd.ClipRect.Y)
+                    );
+
+                    var effect = UpdateEffect(_loadedTextures[drawCmd.TextureId]);
+
+                    foreach (var pass in effect.CurrentTechnique.Passes)
+                    {
+                        pass.Apply();
+
+#pragma warning disable CS0618 // // FNA does not expose an alternative method.
+                        _graphicsDevice.DrawIndexedPrimitives(
+                            primitiveType: PrimitiveType.TriangleList,
+                            baseVertex: vtxOffset,
+                            minVertexIndex: 0,
+                            numVertices: cmdList.VtxBuffer.Size,
+                            startIndex: idxOffset,
+                            primitiveCount: (int)drawCmd.ElemCount / 3
+                        );
+#pragma warning restore CS0618
+                    }
+
+                    idxOffset += (int)drawCmd.ElemCount;
+                }
+
+                vtxOffset += cmdList.VtxBuffer.Size;
+            }
+        }
+    }
+    */
+}
